3863bcb51ccc7023cbcbc0ee345a85d7b11b0eba
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-03-15 14:23:10 -0300

    refactor(cli): extract main fn into lib/main.js

--- cli.js
+++ cli.js
@@ -1,49 +1,12 @@
 #!/usr/bin/env node
 
 const path = require('path')
-const del = require('del')
-const { rollup } = require('rollup')
-const { debug } = require('./lib/util/log')
-const getRollupOptions = require('./lib/rollup-options')
 
-main().catch(error => {
+const cwd = process.cwd()
+const args = process.argv.slice(2)
+const distDir = path.join(cwd, 'dist')
+
+require('./lib/main')({ cwd, args, distDir }).catch(error => {
   console.error(error)
   process.exit(1)
 })
-
-async function main() {
-  const cwd = process.cwd()
-  const args = process.argv.slice(2)
-  const distDir = path.join(cwd, 'dist')
-  debug('init', { cwd, args })
-
-  debug('cleaning dist dir...')
-  await del(distDir)
-  debug('cleaned dist dir')
-
-  // TODO: Buffer log lines and print lines for one task at a time
-  const rollupOptions = getRollupOptions({ cwd })
-  await Promise.all(
-    toArray(rollupOptions).map(
-      async ({ inputOptions, outputOptions }, index) => {
-        const myDebug = debug.bind(null, `[${index}]`)
-        myDebug('inputOptions:', inputOptions)
-        myDebug('outputOptions:', outputOptions)
-        const bundle = await rollup(inputOptions)
-        myDebug('bundle.imports:', bundle.imports)
-        myDebug('bundle.exports:', bundle.exports)
-        await Promise.all(
-          toArray(outputOptions).map(async outputOptions => {
-            myDebug(`building \`${outputOptions.file}\`...`)
-            await bundle.write(outputOptions)
-            myDebug(`built \`${outputOptions.file}\``)
-          }),
-        )
-      },
-    ),
-  )
-}
-
-function toArray(maybeArray) {
-  return [].concat(maybeArray)
-}
