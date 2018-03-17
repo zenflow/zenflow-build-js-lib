a8dbffebb4fae3f148565508809bf70480ca361b
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-03-17 12:20:50 -0300

    test(modules): use named exports instead of default export in fixture

--- tests/fixtures/modules/src/index.js
+++ tests/fixtures/modules/src/index.js
@@ -2,4 +2,5 @@ import SQRT_HALF from 'const-sqrt-half'
 import SQRT2 from 'const-sqrt-two'
 import square from './square'
 
-export default square(SQRT_HALF) + square(SQRT2)
+export const SQR_SQRT_HALF = square(SQRT_HALF)
+export const SQR_SQRT2 = square(SQRT2)
