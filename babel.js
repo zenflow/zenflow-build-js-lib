24b1c8e489c47f812f513e671acd4a372dae4b45
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-03-29 02:28:07 -0300

    fix(babel): fix exported config to be a custom preset

--- babel.js
+++ babel.js
@@ -2,17 +2,17 @@ module.exports = {
   runtimeHelpers: false,
   presets: [
     [
-      require.resolve('babel-preset-env'),
+      require('babel-preset-env'),
       {
         modules: false,
       },
     ],
-    [require.resolve('babel-preset-react')],
+    [require('babel-preset-react')],
   ],
   plugins: [
-    require.resolve('babel-plugin-external-helpers'),
-    require.resolve('babel-plugin-transform-object-rest-spread'),
-    require.resolve('babel-plugin-transform-class-properties'),
+    require('babel-plugin-external-helpers'),
+    require('babel-plugin-transform-object-rest-spread'),
+    require('babel-plugin-transform-class-properties'),
   ],
   exclude: 'node_modules/**',
 }
